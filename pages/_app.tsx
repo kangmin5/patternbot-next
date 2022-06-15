1b87cd8be79f6c04f2cc10e5b5ed259cc43313e2
kangmin5 <mk@kangmin.kr>
2022-06-15 11:04:05 +0900

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import Footer from '@/components/Footer'
+// import Header from '@/components/Header'
+import Layout from '@/components/Layout'
+import Nav from '@/components/Nav'
+import { wrapper } from '@/modules/store'
+import '@/styles/globals.css'
+import { AppProps } from 'next/app'
+
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      {/* <Header /> */}
+      <Nav/>
+      <Layout>
+          <Component {...pageProps} />
+      </Layout> 
+      <Footer/>
+    </div>
+  )
+}
+
+export default wrapper.withRedux(App) 
+// export default App
---
+++ pages/menu/report/orderReport.tsx
@@ -0,0 +1,12 @@
+import OrderReport from '@/components/menu/report/OrderTable'
+import React from 'react'
+
+const OrderReportPage = () => {
+  return (
+      <div>
+        <OrderReport/>
+      </div>
+  )
+}
+
+export default OrderReportPage
